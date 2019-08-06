19132632aea49be25bea4aea97cacedb9394fcb2
deadpool <hanikhellafi@gmail.com>
2019-08-06 14:27:48 -0500

    deploying to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,18 @@
 mysql=require("mysql");
 
-
-var connection=mysql.createConnection({
+var connection;
+if(process.env.JAWSDB_URL){
+    connection=mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+ connection=mysql.createConnection({
     host:"localhost",
     user:"root",
     password:"hani",
     port:3306,
     database:"burger_db"
 
-});
+});}
 
 connection.connect(function(err){
 if(err){
